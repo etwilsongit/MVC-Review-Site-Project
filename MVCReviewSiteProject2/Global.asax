56f5284e9d209f9c94cf64c5fc7bb91831bf29ee
etwilsongit <etwilson.2012@gmail.com>
2017-05-07 22:28:45 -0400

    initial commit after git ignore

---
+++ MVCReviewSiteProject2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCReviewSiteProject2.MvcApplication" Language="C#" %>
---
+++ MVCReviewSiteProject2/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSiteProject2.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+
+    }
+}
