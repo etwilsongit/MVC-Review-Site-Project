fd90981d2d60ca44f92b7eb77da3f86b006cdc4a
etwilsongit <etwilson.2012@gmail.com>
2017-05-08 05:39:10 -0400

    commented throughout program

--- MVCReviewSiteProject2/Models/Category.cs
+++ MVCReviewSiteProject2/Models/Category.cs
@@ -5,15 +5,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace MVCReviewSiteProject2.Models
+namespace MVCReviewSiteProject2.Models //we're using this class as a template
 {
     public class Category
     {
-        [Key]
-        public int ID { get; set; }
+        [Key] //data annotation, works on the field below them
+        public int ID { get; set; } //IDs should ALWAYS be an int
         [Display(Name = "Category Name")]
         public string Name { get; set; }
+        
+        //The bane of my existence, the straw that broke the programs back. The Comment Section. 
         public string Comment { get; set; }
+
 
         public virtual ICollection<Review> Reviews { get; set; }
 
